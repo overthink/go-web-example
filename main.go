c961478ca95cb0d788c790ab709e38393b48a781
Mark Feeney <mark.feeney@gmail.com>
2022-08-03 15:34:06 +0000

    Cleanup, add create task endpoint

--- main.go
+++ main.go
@@ -2,35 +2,75 @@ package main
 
 import (
 	"encoding/json"
+	"mime"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/overthink/go-web-example/taskstore"
 )
 
-// Struct to eventually hold all components needed by this app.
-type taskServer struct {
-	taskStore *taskstore.TaskStore
-}
-
-func (ts *taskServer) pingHandler(w http.ResponseWriter, req *http.Request) {
-	json, err := json.Marshal(map[string]string{"message": "pong"})
+func jsonResponse(w http.ResponseWriter, value interface{}) {
+	js, err := json.Marshal(value)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.Header().Set("Content-type", "application/json")
-	w.Write(json)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
 
-func NewTaskServer() *taskServer {
-	return &taskServer{taskStore: taskstore.New()}
+func handlePing(w http.ResponseWriter, req *http.Request) {
+	jsonResponse(w, map[string]string{"message": "pong"})
+}
+
+// All the components/deps required by the app live in this struct.
+type server struct {
+	taskStore *taskstore.TaskStore
+}
+
+func NewServer() *server {
+	return &server{taskStore: taskstore.New()}
+}
+
+func (s *server) handleCreateTask(w http.ResponseWriter, req *http.Request) {
+	type postData struct {
+		Text string    `json:"text"`
+		Tags []string  `json:"tags"`
+		Due  time.Time `json:"due"`
+	}
+	type response struct {
+		Id int `json:"id"`
+	}
+
+	contentType := req.Header.Get("Content-Type")
+	mediatype, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if mediatype != "application/json" {
+		http.Error(w, "expect application/json Content-Type", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	dec := json.NewDecoder(req.Body)
+	dec.DisallowUnknownFields()
+	var pd postData
+	if err := dec.Decode(&pd); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id := s.taskStore.CreateTask(pd.Text, pd.Tags, pd.Due)
+	jsonResponse(w, response{id})
 }
 
 func main() {
-	server := NewTaskServer()
+	s := NewServer()
 	mux := http.NewServeMux()
-	mux.HandleFunc("/ping", server.pingHandler)
+	mux.HandleFunc("/ping", handlePing)
+	mux.HandleFunc("/task/", s.handleCreateTask)
 	serverPort, ok := os.LookupEnv("SERVERPORT")
 	if !ok {
 		serverPort = "60000"
