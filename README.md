4329af545aa51ba401966ffa55e2763b0e76b53a
Mark Feeney <mark.feeney@gmail.com>
2022-08-03 04:00:49 +0000

    Actually run a server (/ping only)

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # go-web-example
 
 Refresher on setting up REST APIs in go.
+
 Starting with https://eli.thegreenplace.net/2021/rest-servers-in-go-part-1-standard-library/
 
--- main.go
+++ main.go
@@ -1,4 +1,39 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+
+	"github.com/overthink/go-web-example/taskstore"
+)
+
+// Struct to eventually hold all components needed by this app.
+type taskServer struct {
+	taskStore *taskstore.TaskStore
+}
+
+func (ts *taskServer) pingHandler(w http.ResponseWriter, req *http.Request) {
+	json, err := json.Marshal(map[string]string{"message": "pong"})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-type", "application/json")
+	w.Write(json)
+}
+
+func NewTaskServer() *taskServer {
+	return &taskServer{taskStore: taskstore.New()}
+}
+
 func main() {
+	server := NewTaskServer()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", server.pingHandler)
+	serverPort, ok := os.LookupEnv("SERVERPORT")
+	if !ok {
+		serverPort = "60000"
+	}
+	http.ListenAndServe("localhost:"+serverPort, mux)
 }
